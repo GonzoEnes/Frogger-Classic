af3411944129ee4a3124050e148ed9528a9a4e7d
GonzoEnes <93682074+GonzoEnes@users.noreply.github.com>
2023-03-30 23:06:57 +0100

    Add files via upload

---
+++ TP_SO2_Frogger/Frog/structs.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <windows.h>
+#include <tchar.h>
+
+// Todos os ficheiros structs.h serão onde as estruturas do tp vão ser armazenadas. Qualquer outro .h será apenas para declaração de funções p.ex no ficheiro structs.h teríamos uma struct programa {} mas no frog.h teríamos BOOL moveFrog(pFrogs frogs); 
+
